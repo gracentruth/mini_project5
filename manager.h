fa8da3666a9f475ee820c6f4d07947c501b9b905
gracentruth <21900174@handong.edu>
2020-04-18 22:47:53 +0900

    declare searchPricefunc

--- manager.h
+++ manager.h
@@ -3,3 +3,4 @@
 void listProduct(Product *p[],int count); // 전체 등록된 제품 리스트를 출력하는 함수 
 int selectDataNo(Product *p[],int count);// 삭제 또는 수정시 삭제, 수정할 제품을 선택하는 함수
 void searchName(Product *p[], int count);//제품의 이름을 검색해서 제품 정보를 알아내는 함수
+void searchPrice(Product *p[], int count);//제품의 가격을 검색해서 제품 정보를 알아내는 함수
